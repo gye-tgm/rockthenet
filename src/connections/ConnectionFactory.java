1e4d1b5692c6504c8b9f08c62b30755ea6faec75
efrantar-tgm <efrantar@student.tgm.ac.at>
2014-09-28 15:54:31 +0200

    added everything for package refactoring

--- src/connections/ConnectionFactory.java
+++ src/connections/ConnectionFactory.java
@@ -1,7 +1,9 @@
 package connections;
 
 public class ConnectionFactory {
-	public ReadConnection createSNMPv2cConnection(String address, int port, String community) throws ConnectionException {
-		return null;
+	public static ReadConnection createSNMPv2cConnection(String address, int port, String community) throws ConnectionException {
+		ReadConnection connection = new SNMPv2cConnection(address, port, community);
+		connection.establish();
+		return connection;
 	}
 }
