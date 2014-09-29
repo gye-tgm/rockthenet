b419f2ae166e3c96939da94e1d86af8d6ef4a3aa
sschmidt01-tgm <sschmidt01@student.tgm.ac.at>
2014-09-29 22:57:45 +0200

    GUI-Prototype + Settings...

--- src/rockthenet/Main.java
+++ src/rockthenet/Main.java
@@ -1,15 +1,133 @@
 package rockthenet;
 
-import net.percederberg.mibble.MibLoaderException;
-import net.percederberg.mibble.MibValue;
-import rockthenet.firewall.junipernetscreen5gt.JuniperNetscreen5GTRetriever;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import rockthenet.view.Controller;
+import rockthenet.view.NewConnectionDialogController;
+import rockthenet.view.SettingsDialogController;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-public class Main {
-	public static void main(String[] args){
+/**
+ * @author Samuel
+ */
+public class Main extends Application {
 
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Rock the Firewall");
+        initRootLayout();
     }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("view/rockTheNet.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            // Controller stuff
+            Controller controller = loader.getController();
+            controller.setMain(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showSettingsDialog() {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/settingsDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Settings");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // controller stuff
+            SettingsDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showNewConnectionDialog() {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/connectionDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("New Connection");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // controller stuff
+            NewConnectionDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
+
---
+++ src/rockthenet/view/SettingsDialogController.java
@@ -0,0 +1,119 @@
+package rockthenet.view;
+
+/**
+ * Controller for the Settings Dialog
+ * @author Samuel Schmidt
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialogs;
+import rockthenet.EmailValidator;
+
+/**
+ * Dialog to edit the settings
+ *
+ * @author Samuel Schmidt
+ */
+public class SettingsDialogController {
+
+    @FXML
+    private TextField email;
+    @FXML
+    private CheckBox emailNotificationsEnabled;
+    @FXML
+    private TextField refreshIntervall;
+    @FXML
+    private CheckBox refreshIntervallEnabled;
+
+    private Stage dialogStage;
+    private boolean okClicked = false;
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    /**
+     * Sets the person to be edited in the dialog.
+     */
+    public void setFields() {
+        email.setText("");
+        emailNotificationsEnabled.setSelected(true);
+        refreshIntervall.setText("");
+        refreshIntervallEnabled.setSelected(true);
+    }
+
+    /**
+     * Returns true if the user clicked OK, false otherwise.
+     *
+     * @return
+     */
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    private void handleOk() {
+        if (isInputValid()) {
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    /**
+     * Validates the user input in the text fields.
+     *
+     * @return true if the input is valid
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
+        EmailValidator emailValidator = new EmailValidator();
+
+        if (email.getText() == null || email.getText().length() == 0 || !emailValidator.validate(email.getText())) {
+            errorMessage += "Invalid e-mail address!\n";
+        }
+
+        if (refreshIntervall.getText() == null || refreshIntervall.getText().length() == 0) {
+            errorMessage += "Invalid refresh interval!\n";
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Dialogs.create()
+                    .title("Invalid Fields")
+                    .masthead("Please correct invalid fields")
+                    .message(errorMessage)
+                    .showError();
+            return false;
+        }
+    }
+}
+
