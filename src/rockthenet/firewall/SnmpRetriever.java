c01175ba9557a284522e9736cce136e4c95fc4a3
Gary Ye <gye@student.tgm.ac.at>
2014-10-04 17:10:27 +0200

    added refreshPolicies

--- src/rockthenet/firewall/SnmpRetriever.java
+++ src/rockthenet/firewall/SnmpRetriever.java
@@ -6,8 +6,4 @@ import java.util.HashMap;
  * Created by gary on 28/09/14.
  */
 public abstract class SnmpRetriever implements IDataRetriever {
-    @Override
-    public Object get(String name){
-        return null;
-    }
 }
--- src/rockthenet/firewall/junipernetscreen5gt/JuniperNetscreen5GTFirewall.java
+++ src/rockthenet/firewall/junipernetscreen5gt/JuniperNetscreen5GTFirewall.java
@@ -3,11 +3,15 @@ package rockthenet.firewall.junipernetscreen5gt;
 import net.percederberg.mibble.MibLoaderException;
 import rockthenet.firewall.Firewall;
 import rockthenet.firewall.IDataRetriever;
+import rockthenet.firewall.Policy;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
- * Created by gary on 28/09/14.
+ * @author gary
  */
 public class JuniperNetscreen5GTFirewall extends Firewall {
     public JuniperNetscreen5GTFirewall(JuniperNetscreen5GTRetriever dataRetriever, JuniperNetscreen5GTWriter writer) throws IOException, MibLoaderException {
@@ -16,7 +20,8 @@ public class JuniperNetscreen5GTFirewall extends Firewall {
 
     @Override
     public void refreshPolicies() {
-
+        HashMap<Integer, JuniperNetscreen5GTPolicy> h = (HashMap<Integer, JuniperNetscreen5GTPolicy>) dataRetriever.get("policies");
+        setPolicies(new ArrayList<>(h.values()));
     }
 
     @Override
