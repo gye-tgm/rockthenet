47a27145f53696a85f9cfb38e90b901b01277755
Gary Ye <gye@student.tgm.ac.at>
2014-10-09 08:10:55 +0200

    refreshes the polcies at the beginning to bypass

--- src/rockthenet/view/Controller.java
+++ src/rockthenet/view/Controller.java
@@ -74,7 +74,7 @@ public class Controller implements Refreshable {
 
 	@FXML
     private void initialize() {
-        firewall = getTestFirewall(); // TODO: only for testing
+        firewall = getFirewall(); // TODO: only for testing
 
         Image image = new Image(getClass().getResourceAsStream("../resources/refresh-icon.png"));
         refreshButton.setGraphic(new ImageView(image));
@@ -120,6 +120,7 @@ public class Controller implements Refreshable {
         (new Refresher(this)).start();
 
         /* TODO: remove */
+        firewall.refreshPolicies();
         for (Policy policy : firewall.getPolicies())
         	policies.add(new PolicyRow(policy));
         
