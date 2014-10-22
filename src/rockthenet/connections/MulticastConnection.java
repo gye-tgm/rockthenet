37dad82d2b08f603bc86a906b440df113f3cc91e
Gary Ye <gye@student.tgm.ac.at>
2014-10-22 23:22:43 +0200

    Added test and documents for multicast connection

--- src/rockthenet/connections/MulticastConnection.java
+++ src/rockthenet/connections/MulticastConnection.java
@@ -1,12 +1,13 @@
 package rockthenet.connections;
 
-import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 /**
+ * This class provides a multicast connection.
+ *
  * @author Gary Ye
  */
 public class MulticastConnection implements Connection {
@@ -14,21 +15,14 @@ public class MulticastConnection implements Connection {
     private MulticastSocket multicastSocket;
     private int port;
 
-    public MulticastConnection(){}
+    public MulticastConnection() {
+    }
 
     public MulticastConnection(InetAddress group, int port) {
         this.port = port;
         this.group = group;
     }
 
-    /**
-     * Tries to establish a connection to the host/service. (must have been previously initialized)
-     * <p>
-     * <p><i>NOTE:</i> If this method is called while the connection is up, it will simply try to reconnect.
-     * You therefore may also use this method for reconnecting when the connection has been lost.
-     *
-     * @throws rockthenet.connections.ConnectionException thrown if connecting failed
-     */
     @Override
     public void establish() throws ConnectionException {
         try {
@@ -39,18 +33,50 @@ public class MulticastConnection implements Connection {
         }
     }
 
+    @Override
+    public void close() {
+        try {
+            multicastSocket.leaveGroup(group);
+            multicastSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Serializes the given object, which should have implemented Serizliable.
+     *
+     * @param obj the object to serialize.
+     * @return the object as a byte array
+     * @throws IOException
+     */
     public static byte[] serialize(Object obj) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream os = new ObjectOutputStream(out);
         os.writeObject(obj);
         return out.toByteArray();
     }
+
+    /**
+     * Desiralizes the given byte array.
+     *
+     * @param data the given byte array
+     * @return the deserialized object.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
         ByteArrayInputStream in = new ByteArrayInputStream(data);
         ObjectInputStream is = new ObjectInputStream(in);
         return is.readObject();
     }
 
+    /**
+     * Sends the given object to the multicast group. Everyone in the group will receive it.
+     *
+     * @param object the object to send
+     * @throws IOException
+     */
     public void send(Object object) throws IOException {
         byte[] bytes = serialize(object);
 
@@ -58,7 +84,12 @@ public class MulticastConnection implements Connection {
         multicastSocket.send(packet);
     }
 
-    public DatagramPacket receive(){
+    /**
+     * Receives a DatagramPacket from the MulticastGroup
+     *
+     * @return the datagram packet
+     */
+    public DatagramPacket receive() {
         byte[] buf = new byte[1000];
         DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length);
         try {
@@ -69,17 +100,4 @@ public class MulticastConnection implements Connection {
         return datagramPacket;
     }
 
-
-    /**
-     * Closes the connection if it is running; Doesn't do anything otherwise
-     */
-    @Override
-    public void close() {
-        try {
-            multicastSocket.leaveGroup(group);
-            multicastSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
---
+++ src/test/connections/MulticastConnectionTest.java
@@ -0,0 +1,42 @@
+package test.connections;
+
+import org.junit.Before;
+import org.junit.Test;
+import rockthenet.connections.ConnectionException;
+import rockthenet.connections.MulticastConnection;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests the given Multicast Connection class
+ * @author Gary Ye
+ */
+public class MulticastConnectionTest {
+    private MulticastConnection client1, client2;
+    private InetAddress group ;
+    private int port = 6666;
+
+    @Before
+    public void setup() throws ConnectionException, UnknownHostException {
+        group = InetAddress.getByName("224.0.0.1");
+        client1 = new MulticastConnection(group, port);
+        client1.establish();
+
+        client2 = new MulticastConnection(group, port);
+        client2.establish();
+    }
+
+    @Test
+    public void testSend() throws IOException, ClassNotFoundException {
+        String hallo = "abc";
+        client1.send(hallo);
+        DatagramPacket datagramPacket = client2.receive();
+        String received = (String) MulticastConnection.deserialize(datagramPacket.getData());
+        assertEquals(hallo, received);
+    }
+}
