2bdb5f402216eb5fd9bd1c51db1a4a94e2bef9b4
efrantar-tgm <efrantar@student.tgm.ac.at>
2014-10-22 22:07:10 +0200

    added documentation to SessionSettings

--- src/rockthenet/SessionSettings.java
+++ src/rockthenet/SessionSettings.java
@@ -2,10 +2,21 @@ package rockthenet;
 
 import rockthenet.firewall.Firewall;
 
+/**
+ * This is a global singleton class managing the settings of a single session. 
+ * (such as the firewall connected to, the configured refresh interval, ...)
+ * 
+ * @author Elias Frantar
+ * @version 2014-10-22
+ */
 public class SessionSettings {
 	/* this class is a Singleton */
 	private static SessionSettings sessionSettings = new SessionSettings();
-	private SessionSettings() {}
+	private SessionSettings() {} // no instance should be created from outside
+	/**
+	 * Returns the singleton-instance of this class
+	 * @return the only existing instance of this class
+	 */
 	public static SessionSettings getInstance() { return sessionSettings; }
 	
 	/* General Settings */
@@ -17,6 +28,10 @@ public class SessionSettings {
 	private String email = "";
 	private int refreshInterval = 5;
 	
+	/**
+	 * Returns true if this object contains an instance of a {@link Firewall}. ({@code firewall != null})
+	 * @return true if yes; false otherwise
+	 */
 	public boolean isConnected() { return firewall != null; }
 	
 	/* Setters */
