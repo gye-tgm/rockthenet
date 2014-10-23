24794d7d8a5d4e29a6f9b04d53c5d237ee087066
efrantar-tgm <efrantar@student.tgm.ac.at>
2014-10-23 11:04:42 +0200

    it's now possible to execute more than one SSH command

--- src/rockthenet/connections/ssh/SSHConnection.java
+++ src/rockthenet/connections/ssh/SSHConnection.java
@@ -21,6 +21,8 @@ public class SSHConnection implements WriteConnection {
     private static org.apache.log4j.Logger log = Logger.getLogger(SSHConnection.class);
 
     Session session;
+    Channel channel;
+    PrintStream commander;
 	
 	/**
 	 * Creates a new {@link rockthenet.connections.ssh.SSHConnection}
@@ -48,37 +50,31 @@ public class SSHConnection implements WriteConnection {
 	public void establish() throws ConnectionException {
 		try {
 			session.connect();
-		} catch (JSchException e) {
+			
+			/* connect the channel */
+			channel = (ChannelShell) session.openChannel("shell");
+            channel.setOutputStream(System.out, true);
+            channel.connect();
+            
+            commander = new PrintStream(channel.getOutputStream(), true);
+		} catch (JSchException | IOException e) {
 			throw new ConnectionException("connection failed");
 		}
 	}
 
 	@Override
 	public void close() {
+		channel.disconnect();
 		session.disconnect();
+		commander.close();
 	}
 
 	@Override
 	public void execute(String command) throws ConnectionException {
-		try {
-            log.info(command);
-
-			Channel channel = (ChannelShell) session.openChannel("shell");
-            PrintStream commander = new PrintStream(channel.getOutputStream(), true);
+        log.info(command);
             
-            /* connect */
-            channel.setOutputStream(System.out, true);
-            channel.connect();
-            
-            /* execute command */
-            commander.println(command);
-            commander.flush();
-            
-            /* close channel */
-            commander.close();
-			channel.disconnect();
-		} catch (IOException | JSchException e) {
-			throw new ConnectionException("Failed to execute command");
-		}
+        /* execute command */
+        commander.println(command);
+        commander.flush();
 	}
 }
